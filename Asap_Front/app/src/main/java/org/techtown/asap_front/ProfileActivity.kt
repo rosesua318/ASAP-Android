57b5f7c32cbdbb0a9cf58e61c2dce8050221e10f
rosesua318 <rosesua@duksung.ac.kr>
2021-11-04 18:03:34 +0900

    profile, comment UI

---
+++ Asap_Front/app/src/main/java/org/techtown/asap_front/ProfileActivity.kt
@@ -0,0 +1,11 @@
+package org.techtown.asap_front
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.profile_activity)
+    }
+}
