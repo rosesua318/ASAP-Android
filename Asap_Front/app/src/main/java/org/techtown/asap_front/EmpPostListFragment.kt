9fcd2ebb22b5f128be2ff0b0bb4a5c89387e6405
juserh <rosapark07@gmail.com>
2021-11-10 02:34:20 +0900

    bottom navigation view 추가

--- Asap_Front/app/src/main/java/org/techtown/asap_front/EmpPostListFragment.kt
+++ Asap_Front/app/src/main/java/org/techtown/asap_front/EmpPostListFragment.kt
@@ -13,10 +13,11 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [MyPageFragment.newInstance] factory method to
+ * Use the [EmpPostListFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class MyPageFragment : Fragment() {
+class EmpPostListFragment : Fragment() {
+
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -34,7 +35,7 @@ class MyPageFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.mypage_fragment, container, false)
+        return inflater.inflate(R.layout.emp_post_list_fragment, container, false)
     }
 
     companion object {
@@ -44,12 +45,12 @@ class MyPageFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment MyPageFragment.
+         * @return A new instance of fragment EmpPostListFragment.
          */
         // TODO: Rename and change types and number of parameters
         //@JvmStatic
-        fun newInstance(): MyPageFragment{
-            return MyPageFragment()
+        fun newInstance(): EmpPostListFragment{
+            return EmpPostListFragment()
         }
     }
 }
--- Asap_Front/app/src/main/java/org/techtown/asap_front/JobPostListFragment.kt
+++ Asap_Front/app/src/main/java/org/techtown/asap_front/JobPostListFragment.kt
@@ -13,10 +13,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [MyPageFragment.newInstance] factory method to
+ * Use the [JobPostListFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class MyPageFragment : Fragment() {
+class JobPostListFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -29,12 +29,10 @@ class MyPageFragment : Fragment() {
         }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.mypage_fragment, container, false)
+        return inflater.inflate(R.layout.job_post_list_fragment, container, false)
     }
 
     companion object {
@@ -44,12 +42,11 @@ class MyPageFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment MyPageFragment.
+         * @return A new instance of fragment JobPostListFragment.
          */
         // TODO: Rename and change types and number of parameters
         //@JvmStatic
-        fun newInstance(): MyPageFragment{
-            return MyPageFragment()
+        fun newInstance(): JobPostListFragment{
+            return JobPostListFragment()
         }
-    }
 }
