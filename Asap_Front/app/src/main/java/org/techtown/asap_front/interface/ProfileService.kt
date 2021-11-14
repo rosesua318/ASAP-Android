ffd51273db4fa3e1fe089c774a062bab0cdd168b
rosesua318 <rosesua@duksung.ac.kr>
2021-11-14 20:30:11 +0900

    추천하기, 프로필 기능 연결

---
+++ Asap_Front/app/src/main/java/org/techtown/asap_front/interface/ProfileService.kt
@@ -0,0 +1,15 @@
+package org.techtown.asap_front.`interface`
+
+import org.techtown.asap_front.data_object.Profile
+import retrofit2.Call
+import retrofit2.http.FormUrlEncoded
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface ProfileService {
+    @FormUrlEncoded
+    @GET("/users/profiles/{id}")
+    fun getProfile(
+        @Path("id") id: Int
+    ) : Call<Profile>
+}
